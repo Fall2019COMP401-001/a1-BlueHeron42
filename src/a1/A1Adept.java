4144314a78cd0645b07d29f8e743324f665481ee
Andrew Chun <achun@live.unc.edu>
2019-09-02 05:12:11 -0400

    Completed Adept?

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -2,13 +2,66 @@ package a1;
 
 import java.util.Scanner;
 
-public class A1Adept {
+public class A1Adept{
 
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		int numberOfItems = scan.nextInt();
+		String itemNames [] = new String[numberOfItems];
+		double itemPrices [] = new double[numberOfItems];
+		for (int i = 0; i < numberOfItems; i++) {
+			itemNames[i] = scan.next();
+			itemPrices[i] = scan.nextDouble();
+		}
+		int customerCount = scan.nextInt();
+		String customerNames [] = new String[customerCount * 2];
+		double customerCosts [] = new double[customerCount];
+		for (int i = 0; i < customerCount; i++) {
+			double customerTotal = 0.0;
+			customerNames[2*i] = scan.next();
+			customerNames[(2*i)+1] = scan.next();
+			int itemCount = scan.nextInt();
+			for (int j = 0; j < itemCount; j++) {
+				int countOfEachItem = scan.nextInt();
+				//System.out.println(countOfEachItem);
+				String item = scan.next();
+				for (int k = 0; k < numberOfItems; k++) {
+					if(item.equals(itemNames[k])) {
+						customerTotal += countOfEachItem * itemPrices[k];
+					}
+				}
+				
+			}
+			customerCosts[i] = customerTotal;
+		}
+		double biggest = customerCosts[0];
+		String biggestName;
+		int o = 0;
+		for(o = 0; o<customerCount; o++) {
+			if(customerCosts[o] > biggest) {
+				biggest = customerCosts[o];
+			}
+		}
 
-		// Your code follows here.
-		
+		System.out.println("Biggest: "+ customerNames[2*(o-1)] + " " + customerNames[2*(o-1)+1] + " (" + biggest + ")");
+
+		double smallest = customerCosts[0];
+		int l = 0;
+		for(l = 1; l < customerCount; l++) {
+			if(customerCosts[l] < smallest) {
+				smallest = customerCosts[l];
+			}
+		}
+		System.out.println("Smallest: "+ customerNames[2*(l-1)] + " " + customerNames[2*(l-1)+1] + " (" + smallest + ")");
+
+		double totalCost= 0;
+		for(int a = 0; a < customerCount; a++) {
+			totalCost += customerCosts[a];
+		}
+		//System.out.println(totalCost);
+		double average = totalCost/customerCount;
+		System.out.println("Average: " + average);
 	}
+
 }
