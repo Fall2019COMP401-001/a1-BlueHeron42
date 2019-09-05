860540472f19b2a658deab545c2a4cc5d4135e58
Andrew Chun <achun@live.unc.edu>
2019-09-05 02:21:46 -0400

    FINISHED NOVICE

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -3,12 +3,26 @@ package a1;
 import java.util.Scanner;
 
 public class A1Novice {
+	
 
 	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		Scanner sc = new Scanner(System.in);
+		int customerCount = sc.nextInt();
+		String [] firstNames = new String[customerCount];
+		String [] lastNames = new String[customerCount];
+		double [] customerTotals = new double[customerCount];
+		for(int i = 0; i < customerCount; i++) {
+			firstNames[i] = sc.next();
+			lastNames[i] = sc.next();
+			int quantityBought = sc.nextInt();
+			for(int a = 0; a < quantityBought; a++) {
+				int numberBought = sc.nextInt();
+				String item = sc.next();
+				double price = sc.nextDouble();
+				customerTotals[i] += price * numberBought;
+			}
+			System.out.println(firstNames[i].charAt(0)+". "+ lastNames[i]+": "+ String.format("%.2f",  customerTotals[i]));
+		}
 		
 	}
 }
